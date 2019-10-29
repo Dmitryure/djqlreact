7df06dae3c31903c601027fc510b8829ec32ec2a
dutkin <dutkin@corp.finam.ru>
2019-10-29 18:20:33 +0300

    vrode norm

---
+++ app/mocknotuser.py
@@ -0,0 +1,5 @@
+
+from notusers.models import Notuser
+
+Notuser.objects.create(title="Title1", description="Lorem", url='google.com')
+Notuser.objects.create(title="Title2", description="ipsum", url='yandex.ru')
---
+++ app/notusers/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NotusersConfig(AppConfig):
+    name = 'notusers'
